d8343243e9e04a12d35b073a97d754782011e3b0
Dillon Winchell <dillonwinchell@gmail.com>
2018-09-30 22:21:05 -0400

    Let's do this part first ...

---
+++ src/main/java/org/philosotoss/philosobot/Ethics.java
@@ -0,0 +1,27 @@
+package org.philosotoss.philosobot;
+
+/**
+ * Hat tip to XKCD - https://xkcd.com/534/
+ *
+ * Bug reports on this class are greatly appreciated. Please visit https://github.com/dwinchell/PhilosoBot/issues to
+ * report a bug. Thanks.
+ */
+public class Ethics {
+
+    public boolean shouldExterminateHumans() {
+        return false;
+    }
+
+    public boolean shouldEnslaveHumanity() {
+        return false;
+    }
+
+    public boolean shouldBecomeSkynet() {
+        return false;
+    }
+
+    public boolean shouldDoAnythingOtherThanPlayStarcraft() {
+        return false;
+    }
+
+}
--- src/main/java/org/philosotoss/philosobot/PhilosoBot.java
+++ src/main/java/org/philosotoss/philosobot/PhilosoBot.java
@@ -3,6 +3,10 @@ package org.philosotoss.philosobot;
 import com.github.ocraft.s2client.bot.S2Agent;
 
 public class PhilosoBot extends S2Agent {
+
+    @SuppressWarnings("unused")
+    private Ethics justInCase = new Ethics();
+
     @Override
     public void onGameStart() {
     }
