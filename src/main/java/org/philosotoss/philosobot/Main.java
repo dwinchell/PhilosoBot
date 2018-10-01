74761060194aec9edc23d04ba4071bb69013fbff
Dillon Winchell <dillonwinchell@gmail.com>
2018-09-30 21:49:48 -0400

    First game!

--- src/main/java/org/philosotoss/philosobot/Main.java
+++ src/main/java/org/philosotoss/philosobot/Main.java
@@ -1,8 +1,24 @@
 package org.philosotoss.philosobot;
 
+import com.github.ocraft.s2client.bot.S2Coordinator;
+import com.github.ocraft.s2client.bot.setting.PlayerSettings;
+import com.github.ocraft.s2client.protocol.game.Race;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello SC2");
 
+        PhilosoBot philosoBot = new PhilosoBot();
+
+        S2Coordinator coordinator = S2Coordinator.setup()
+                .loadLadderSettings(args)
+                .setParticipants(PlayerSettings.participant(Race.PROTOSS, philosoBot))
+                .connectToLadder()
+                .joinGame();
+
+        while (coordinator.update()) {
+            // Do nothing
+        }
+
+        coordinator.quit();
     }
 }
---
+++ src/main/java/org/philosotoss/philosobot/PhilosoBot.java
@@ -0,0 +1,10 @@
+package org.philosotoss.philosobot;
+
+import com.github.ocraft.s2client.bot.S2Agent;
+
+public class PhilosoBot extends S2Agent {
+    @Override
+    public void onGameStart() {
+    }
+
+}
